26be3cc57bc14cbcc541dfc2243e2eaacd15c9b7
Vinicius Dias <carlosv775@gmail.com>
2020-10-17 13:23:17 -0300

    Vídeo 2.2

--- manipulacao.php
+++ manipulacao.php
@@ -5,11 +5,17 @@ $dados = require 'dados.php';
 $contador = count($dados);
 echo "Número de países: $contador\n";
 
-function convertePaisParaLetraMaisucula(array $pais) {
+function convertePaisParaLetraMaisucula(array $pais): array {
     $pais['pais'] = mb_convert_case($pais['pais'], MB_CASE_UPPER);
     return $pais;
 }
 
+function verificaSePaisTemEspacoNoNome(array $pais): bool
+{
+    return strpos($pais['pais'], ' ') !== false;
+}
+
 $dados = array_map('convertePaisParaLetraMaisucula', $dados);
+$dados = array_filter($dados, 'verificaSePaisTemEspacoNoNome');
 
 var_dump($dados);
