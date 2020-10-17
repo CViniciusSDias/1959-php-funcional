e35b4cd7b87d5ccbce65e89f3b58ef8fefc46fb2
Vinicius Dias <carlosv775@gmail.com>
2020-10-17 13:43:23 -0300

    Vídeo 2.3

--- manipulacao.php
+++ manipulacao.php
@@ -5,6 +5,10 @@ $dados = require 'dados.php';
 $contador = count($dados);
 echo "Número de países: $contador\n";
 
+function somaMedalhas(int $medalhasAcumuladas, int $medalhas) {
+    return $medalhasAcumuladas + $medalhas;
+}
+
 function convertePaisParaLetraMaisucula(array $pais): array {
     $pais['pais'] = mb_convert_case($pais['pais'], MB_CASE_UPPER);
     return $pais;
@@ -15,7 +19,13 @@ function verificaSePaisTemEspacoNoNome(array $pais): bool
     return strpos($pais['pais'], ' ') !== false;
 }
 
+function medalhasAcumuladas(int $medalhasAcumuladas, array $pais): int {
+    return $medalhasAcumuladas + array_reduce($pais['medalhas'], 'somaMedalhas', 0);
+}
+
 $dados = array_map('convertePaisParaLetraMaisucula', $dados);
 $dados = array_filter($dados, 'verificaSePaisTemEspacoNoNome');
 
 var_dump($dados);
+
+echo array_reduce($dados, 'medalhasAcumuladas', 0);
